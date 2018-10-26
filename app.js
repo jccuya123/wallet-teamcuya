f28b32f4f712b83273d67e528f6a45c5c106912a
JC <jccuya123>
2018-10-26 09:15:23 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+(function() {
+  var app = new Vue({
+    el: '#app',
+    data: {
+    id: null,
+    accountNumber: null,
+    name: null,
+    balance: null,
+    pin: null,
+    accounts: []
+    },
+    created: function() {
+    var self = this;
+    axios.get('http://localhost:3300/api')
+      .then(function(res) {
+        self.accounts = res.data;
+      })
+      .catch(function(err) {
+        self.accounts = [];
+      });
+    },
+    methods: {
+    clear: function() {
+      this.id = null,
+      this.accountNumber = null,
+      this.name = null,
+      this.balance = null,
+      this.pin = null  
+    },
+    }
+  });
+console.log(cardoVue);
+})();
---
+++ index.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var PORT = 3300;
+var simpleJsonStore = require('simple-json-store');
+
+var store = new simpleJsonStore('./data.json', { accounts: [] });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//home html
+app.get('/', (req, res) => { 
+  res.render('../fund.pug');
+});
+
+//all users
+app.get('/api/', (req,res) => {
+  var accounts = store.get('accounts'); 
+  res.json(accounts);
+})
+
+//specific user
+app.get('/api/:id', (req, res) => {
+  var id = req.param.id;
+  var all = store.get('accounts');
+  var account = all.find(account => all.id == id);
+  res.json(account);
+});
+
+//add user
+app.post('/api/', (req, res) => {
+  var all = store.get('accounts');
+  var newUser = {
+    id: all.length > 0 ? Number(all[all.length - 1].id) + Number(1) : 1,
+    accountNumber: req.body.accountNumber,
+    name: req.body.name,
+    balance: req.body.balance,
+    pin: req.body.pin
+  };
+  
+  all.push(newUser);
+  store.set('accounts',all);
+  res.json(store.get('accounts'));
+});
+
+//update user
+app.put('/api/:id', (req,res) => {
+  var id = req.params.id;
+  var all = store.get('accounts');
+  let note = {};
+  
+  for (let i = 0; i < all.length; i++) {
+    if (all[i].id == id) {
+      all[i].accountNumber = req.body.accountNumber;
+      all[i].name = req.body.name;
+      all[i].balance = req.body.balance;
+      all[i].pin = req.body.pin;
+      note = all[i];
+      break;
+    }
+  }
+
+  store.set('accounts', all);
+  res.json(note);
+});
+
+app.set('view engine', 'pug');
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
